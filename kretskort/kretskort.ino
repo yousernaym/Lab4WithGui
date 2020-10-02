72fd704cd098b19712ea113a8cf1fab2f7ac980d
Yousernaym <mickeh.se@hotmail.com>
2020-10-02 16:11:20 +0200

    Dropped the idea of remote states to reduce complexity. Refactored.

--- kretskort/kretskort.ino
+++ kretskort/kretskort.ino
@@ -1,56 +1,27 @@
-//Använder man flera ino-filer läggs allt ihop till en enda fil, vilket kan skapa en del problem,
-//,så jag gjorde ett zip library med cpp/h-filer.
-//Känns knasigt att behöva göra ett lib bara för att få vettig struktur på koden, men men...
+//Disclaimer 1:
+//Är inte van att skriva icke-OOP-kod men valde bort C++ för att träna på "riktig" embedded kodning.
+
+//Disclaimer 2:
+//Har man flera ino-filer slås allt ihop till en enda fil vilket kan skapa en del problem,
+//så jag gjorde ett zip library med cpp/h-filer.
+//Känns knasigt att behöva göra ett lib för att få struktur på koden, men men...
 
 #include <stdbool.h>
-#include "kretskort.h"
-#include "localStates.h"
-#include "remoteStates.h"
-
-enum STATE {STATE_LOCAL, STATE_REMOTE};
-
-static volatile STATE state;
-static volatile char localState;
-
-//Reset all color components to off
-void lightsOut()
-{
-  for (int i = FIRST_RGB_PIN; i < FIRST_RGB_PIN + 3; i++)
-    digitalWrite(i, LOW);  
-}
-
-void button0Change()
-{
-  if (button0Click(&localState))
-     state = STATE_LOCAL;
-}
-
-void button1Change()
-{
-  if (button1Click(&localState))
-    state = STATE_LOCAL;
-}
+#include "deviceStates.h"
+#include "serial.h"
 
 void setup()
 {
-  initButtons();
-  
-  pinMode(FIRST_RGB_PIN, OUTPUT);
-  pinMode(FIRST_RGB_PIN + 1, OUTPUT);
-  pinMode(FIRST_RGB_PIN + 2, OUTPUT);
-  
+  initDeviceStates();
   Serial.begin(9600);
-  lightsOut();
-
-  attachInterrupt(digitalPinToInterrupt(BUTTON0_PIN), button0Change, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(BUTTON1_PIN), button1Change, CHANGE);
 }
 
 void loop() 
 {
-  if (state == STATE_REMOTE)
-    processRemoteState();
-  else if (state == STATE_LOCAL)
-    processLocalState(localState);
-
+  if (Serial.available())
+  {
+    char c = Serial.read();
+  }
+  readSerialCommand();
+  processCurrentState();  
 }
